fc77e91dabddb1674b1fb1654ae73705c0b2f4d4
Mark Needham <mark.needham@neotechnology.com>
2016-09-11 14:53:29 +0100

    initial commit

---
+++ modeling/guides/render.sh
@@ -0,0 +1,32 @@
+echo "Usage: sh render.sh [publish]"
+GUIDES=../../neo4j-guides
+# git clone http://github.com/jexp/neo4j-guides $GUIDES
+
+function render {
+  $GUIDES/run.sh index.adoc index.html +1 "$@"
+  $GUIDES/run.sh 01_flight_dataset.adoc 01_flight_dataset.html +1 "$@"
+  $GUIDES/run.sh 02_flight.adoc 02_flight.html +1 "$@"
+  # $GUIDES/run.sh 03_got_houses.adoc 03_got_houses.html +1 "$@"
+  # $GUIDES/run.sh 04_got_families.adoc 04_got_families.html +1 "$@"
+}
+
+# -a env-training is a flag to enable full content, if you comment it out, the guides are rendered minimally e.g. for a presentation
+if [ "$1" == "publish" ]; then
+  URL=guides.neo4j.com/modeling-airports
+  render http://$URL -a csv-url=https://raw.githubusercontent.com/mneedham/neo4j-got/master/data/import -a env-training
+  s3cmd put --recursive -P *.html img s3://${URL}/
+  s3cmd put -P index.html s3://${URL}
+
+  # URL=guides.neo4j.com/reco/file
+  # render http://$URL -a env-training -a csv-url=file:///
+  # s3cmd put --recursive -P *.html img s3://${URL}/
+  # s3cmd put -P index.html s3://${URL}
+  echo "Publication Done"
+else
+  URL=localhost:8001
+# copy the csv files to $NEO4J_HOME/import
+  render http://$URL -a csv-url=file:// -a env-training
+  echo "Starting Websever at $URL Ctrl-c to stop"
+  python $GUIDES/http-server.py
+  # python -m SimpleHTTPServer
+fi
