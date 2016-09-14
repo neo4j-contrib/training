31f3ecabd4290429e332ac78a353aa3742c73488
Michael Hunger <github@jexp.de>
2016-09-14 07:07:28 +0200

    added initial fundamentals guides

---
+++ fundamentals/guides.sh
@@ -0,0 +1,21 @@
+GUIDES=`pwd`/../neo4j-guides
+URL=guides.neo4j.com/fundamentals
+
+function create_guide {
+   cd $1
+   $GUIDES/run.sh $2 $3 +1 http://$URL -a guides=http://guides.neo4j.com/fundamentals
+
+   s3cmd put -P $3 s3://$URL/
+   mv $3 /tmp/
+   if [ "$4" != "" ]; then
+   echo "Images" $4
+   echo s3cmd put -P $4 s3://$URL/img/
+   s3cmd put -P $4 s3://$URL/img/
+   fi
+   cd -
+}
+
+create_guide guides basics.adoc basics.html "img/*.png"
+create_guide guides advanced.adoc advanced.html 
+create_guide guides updates.adoc updates.html 
+create_guide "guides" guides.adoc index.html 
