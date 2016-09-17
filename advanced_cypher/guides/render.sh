1cdd12adf420732841cc663376e54490b4471e14
Mark Needham <mark.needham@neotechnology.com>
2016-09-17 12:09:34 +0100

    structure for advanced Cypher

---
+++ advanced_cypher/guides/render.sh
@@ -0,0 +1,32 @@
+echo "Usage: sh render.sh [publish]"
+GUIDES=../../neo4j-guides
+# git clone http://github.com/jexp/neo4j-guides $GUIDES
+
+function render {
+  $GUIDES/run.sh index.adoc index.html +1 "$@"
+  $GUIDES/run.sh 01_cypher_refresher.adoc 01_cypher_refresher.html +1 "$@"
+  $GUIDES/run.sh 02_forcing_index_usage.adoc 02_forcing_index_usage.html +1 "$@"
+  $GUIDES/run.sh 03_procedures.adoc 03_procedures.html +1 "$@"
+  $GUIDES/run.sh 04_intermediate_state.adoc 04_intermediate_state.html +1 "$@"
+}
+
+# -a env-training is a flag to enable full content, if you comment it out, the guides are rendered minimally e.g. for a presentation
+if [ "$1" == "publish" ]; then
+  URL=guides.neo4j.com/advanced-cypher
+  render http://$URL -a csv-url=https://raw.githubusercontent.com/mneedham/neo4j-got/master/data/import -a env-training
+  s3cmd put --recursive -P *.html img s3://${URL}/
+  s3cmd put -P index.html s3://${URL}
+
+  # URL=guides.neo4j.com/reco/file
+  # render http://$URL -a env-training -a csv-url=file:///
+  # s3cmd put --recursive -P *.html img s3://${URL}/
+  # s3cmd put -P index.html s3://${URL}
+  echo "Publication Done"
+else
+  URL=localhost:8001
+# copy the csv files to $NEO4J_HOME/import
+  render http://$URL -a csv-url=file:// -a env-training
+  echo "Starting Websever at $URL Ctrl-c to stop"
+  python $GUIDES/http-server.py
+  # python -m SimpleHTTPServer
+fi
