a878618b33bc1a4df71efecd941f9c579f100e6a
Mark Needham <mark.needham@neotechnology.com>
2016-09-17 14:41:58 +0100

    inject CSV URI

--- modeling/guides/render.sh
+++ modeling/guides/render.sh
@@ -12,12 +12,12 @@ function render {
 
 # -a env-training is a flag to enable full content, if you comment it out, the guides are rendered minimally e.g. for a presentation
 if [ "$1" == "publish" ]; then
-  URL=guides.neo4j.com/modeling-airports
-  render http://$URL -a csv-url=https://raw.githubusercontent.com/mneedham/neo4j-got/master/data/import -a env-training
+  URL=guides.neo4j.com/modeling_airports
+  render http://$URL -a csv-url=https://raw.githubusercontent.com/neo4j-contrib/training/master/modeling/data/ -a env-training
   s3cmd put --recursive -P *.html img s3://${URL}/
   s3cmd put -P index.html s3://${URL}
 
-  # URL=guides.neo4j.com/reco/file
+  # URL=guides.neo4j.com/modeling-airports/file
   # render http://$URL -a env-training -a csv-url=file:///
   # s3cmd put --recursive -P *.html img s3://${URL}/
   # s3cmd put -P index.html s3://${URL}
