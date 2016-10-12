0312bc327f3a1d465455a91440b19d637a94fc80
Mark Needham <mark.needham@neotechnology.com>
2016-10-12 09:42:51 -0700

    flight -> connection

---
+++ src/main/java/SimpleDurationParser.java
@@ -0,0 +1,38 @@
+public class SimpleDurationParser
+{
+    public static double parseTextToMillis( String duration )
+    {
+        String[] fields = duration.split( "[^-+\\d\\.]" );
+        double value = Double.parseDouble( fields[0] );
+        String units = duration.substring( fields[0].length() ).toLowerCase();
+        if ( units.startsWith( "ms" ) )
+        {
+            return value;
+        }
+        else if ( units.startsWith( "milli" ) )
+        {
+            return value;
+        }
+        else if ( units.startsWith( "s" ) )
+        {
+            return value * 1000.0;
+        }
+        else if ( units.startsWith( "m" ) )
+        {
+            return value * 1000.0 * 60.0;
+        }
+        else if ( units.startsWith( "h" ) )
+        {
+            return value * 1000.0 * 60.0 * 60.0;
+        }
+        else if ( units.startsWith( "d" ) )
+        {
+            return value * 1000.0 * 60.0 * 60.0 * 24.0;
+        }
+        else
+        {
+            throw new IllegalArgumentException(
+                    "Unknown units specified in '" + duration + "' - supported units are: (ms, s, m, h, d)" );
+        }
+    }
+}
---
+++ src/main/java/TrainingProcedures.java
@@ -0,0 +1,109 @@
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.kernel.api.exceptions.InvalidArgumentsException;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Mode;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+import org.neo4j.procedure.TerminationGuard;
+import org.neo4j.procedure.UserFunction;
+
+import static org.neo4j.helpers.collection.MapUtil.map;
+import static org.neo4j.procedure.Mode.READ;
+import static org.neo4j.procedure.Mode.WRITE;
+
+public class TrainingProcedures
+{
+    @Context
+    public GraphDatabaseService db;
+
+    @Context
+    public TerminationGuard terminationGuard;
+
+    @Description("Find movies by an actor")
+    @Procedure(name = "training.moviesOnly", allowed = "movies_only", mode = READ)
+    public Stream<Movie> moviesOnly( @Name("name") String name )
+            throws InvalidArgumentsException, IOException
+    {
+        String query = "MATCH (:Person {name: {name}})-[:ACTED_IN]->(movie) RETURN movie";
+        return db.execute( query, map( "name", name ) )
+                .stream()
+                .map( Movie::new );
+    }
+
+
+    @Procedure(name = "training.writeProcedure", mode = WRITE, allowed = "allowed_role")
+    public Stream<StringResult> writeProcedure()
+    {
+        return db.execute( "CREATE (p:ProcCreated) RETURN labels(p) AS labels" ).stream().map(
+                map -> new StringResult( ":" + ((List<?>) map.get( "labels" )).get( 0 ).toString() ) );
+    }
+
+    @Procedure(name = "training.waitFor", mode = READ, allowed = "allowed_role")
+    public void waitFor(
+            @Name(value = "duration", defaultValue = "1000ms") String durationText,
+            @Name(value = "step", defaultValue = "500ms") String stepText )
+    {
+        waitForSpecifiedDuration(
+                SimpleDurationParser.parseTextToMillis( durationText ),
+                SimpleDurationParser.parseTextToMillis( stepText ) );
+    }
+
+    @UserFunction(name = "training.waitFor", allowed = "allowed_role")
+    public long waitForFunction(
+            @Name(value = "duration", defaultValue = "1000ms") String durationText,
+            @Name(value = "step", defaultValue = "500ms") String stepText )
+    {
+        return waitForSpecifiedDuration(
+                SimpleDurationParser.parseTextToMillis( durationText ),
+                SimpleDurationParser.parseTextToMillis( stepText ) );
+    }
+
+    private long waitForSpecifiedDuration( double duration, double step )
+    {
+        long start = System.currentTimeMillis();
+        long end = start;
+        try
+        {
+            while ( end - start < duration )
+            {
+                Thread.sleep( (long) step );
+                end = System.currentTimeMillis();
+                terminationGuard.check();
+            }
+        }
+        catch ( InterruptedException e )
+        {
+            e.printStackTrace();
+        }
+        return end - start;
+    }
+
+    public class StringResult
+    {
+        public final String value;
+
+        public StringResult( String value )
+        {
+            this.value = value;
+        }
+    }
+
+    public static class Movie
+    {
+        public String title;
+
+        public Movie( Map<String, Object> row )
+        {
+            Node movie = (Node) row.get( "movie" );
+            this.title = movie.getProperty( "title" ).toString();
+        }
+    }
+}
+
