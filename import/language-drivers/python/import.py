13c826459538b2adb310ea0cf29b6aaea9a8e105
Mark Needham <mark.needham@neotechnology.com>
2017-10-09 13:55:31 +0100

    new folder structure

---
+++ import/language-drivers/python/import.py
@@ -0,0 +1,23 @@
+from neo4j.v1 import GraphDatabase, basic_auth
+import sys
+
+password = "neo"
+
+query = """\
+MERGE (p1:Person {name: {person1} })
+MERGE (p2:Person {name: {person2} })
+MERGE (p1)-[:KNOWS]->(p2)
+"""
+
+def import_data(person1, person2):
+    with GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", password)) as driver:
+        with driver.session() as session:
+            result = session.run(query, {"person1": person1, "person2": person2})
+            print(result.summary().counters);
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python import.py [Person1] [Person2]")
+        sys.exit(1)
+
+    import_data(sys.argv[1], sys.argv[2])
