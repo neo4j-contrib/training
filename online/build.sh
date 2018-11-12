24cd8c850c644591e04cef697b7f673bde3fec26
Ryan Boyd <ryan@ryguy.com>
2018-11-12 13:50:04 -0800

    moved image references

--- online/build.sh
+++ online/build.sh
@@ -2,4 +2,4 @@ echo "Rendering Cypher Training"
 asciidoctor -T _templates cypher/index_60-Minute-Cypher.adoc -o cypher/index_wp.html
 asciidoctor -a image=$IMG -a env-training -T _templates/parts cypher/part*.adoc -D cypher/html/
 echo "Rendering Production Training"
-asciidoctor -T _production_templates production/index_neo4j_in_production.adoc -o production/index_wp.html
+asciidoctor -a image=$IMG_PROD -T _production_templates production/index_neo4j_in_production.adoc -o production/index_wp.html
--- online/build_publish.sh
+++ online/build_publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export IMG='https://s3.amazonaws.com/dev.assets.neo4j.com/wp-content/uploads/online/graphdatabases'
+export IMG_PROD='https://s3.amazonaws.com/dev.assets.neo4j.com/wp-content/uploads/online/production'
+
 echo "Building---"
 ./build.sh
 echo "Publishing---"
