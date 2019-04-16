bc5534858fadaee53de1b4defd247057fb30dc25
johnymontana <lyonwj@gmail.com>
2019-04-16 07:02:00 -0400

    use https for linking guides

--- graph_algorithms/guides/render.sh
+++ graph_algorithms/guides/render.sh
@@ -19,14 +19,14 @@ function render {
 # -a env-training is a flag to enable full content, if you comment it out, the guides are rendered minimally e.g. for a presentation
 if [ "$1" == "publish" ]; then
   URL=guides.neo4j.com/graph_algorithms
-  render http://$URL -a env-training
+  render https://$URL -a env-training
   s3cmd put --recursive -P *.html img s3://${URL}/
   s3cmd put -P index.html s3://${URL}
   echo "Publication Done"
 else
   URL=localhost:8001
 # copy the csv files to $NEO4J_HOME/import
-  render http://$URL -a env-training
+  render https://$URL -a env-training
   echo "Starting Websever at $URL Ctrl-c to stop"
   python $GUIDES/http-server.py
   # python -m SimpleHTTPServer
