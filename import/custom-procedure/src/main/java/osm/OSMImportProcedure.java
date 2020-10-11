1280c076c01e8053e9aa40b928d534e104bebb70
Michael Hunger <github@jexp.de>
2020-10-11 03:40:03 +0200

    Import Training updates

--- import/custom-procedure/src/main/java/osm/OSMImportProcedure.java
+++ import/custom-procedure/src/main/java/osm/OSMImportProcedure.java
@@ -1,108 +1,87 @@
 package osm;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Stream;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
+import org.neo4j.graphdb.*;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Mode;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.procedure.Context;
-import org.neo4j.procedure.Mode;
-import org.neo4j.procedure.Name;
-import org.neo4j.procedure.Procedure;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
-public class OSMImportProcedure
-{
-    public static final RelationshipType EDITED = RelationshipType.withName( "EDITED" );
+public class OSMImportProcedure {
+    public static final RelationshipType EDITED = RelationshipType.withName("EDITED");
     @Context
-    public GraphDatabaseService db;
-    private static Label POINT = Label.label( "Point" );;
-    private static Label USER = Label.label( "User" );;
+    public Transaction tx;
+    private static Label POINT = Label.label("Point");
+    ;
+    private static Label USER = Label.label("User");
+    ;
 
-    public static class Summary
-    {
+    public static class Summary {
         public String value;
 
-        public Summary( String value )
-        {
-            this.value= value;
+        public Summary(String value) {
+            this.value = value;
         }
     }
 
     @Procedure(mode = Mode.WRITE)
-    public Stream<Summary> importUri( @Name("uri") String uri ) throws ParserConfigurationException, IOException,
-            SAXException, XPathExpressionException
-    {
+    public Stream<Summary> importUri(@Name("uri") String uri) throws ParserConfigurationException, IOException,
+            SAXException, XPathExpressionException {
         DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-        domFactory.setNamespaceAware( true );
+        domFactory.setNamespaceAware(true);
         DocumentBuilder builder = domFactory.newDocumentBuilder();
-        Document doc = builder.parse( uri );
+        Document doc = builder.parse(uri);
         XPath xpath = XPathFactory.newInstance().newXPath();
 
-        XPathExpression nodeExpression = xpath.compile( "//node" );
-        NodeList nodes = (NodeList) nodeExpression.evaluate( doc, XPathConstants.NODESET );
+        XPathExpression nodeExpression = xpath.compile("//node");
+        NodeList nodes = (NodeList) nodeExpression.evaluate(doc, XPathConstants.NODESET);
 
         Map<String, Node> users = new HashMap<>();
 
-        for ( int i = 0; i < nodes.getLength(); i++ )
-        {
-            org.w3c.dom.Node node = nodes.item( i );
+        for (int i = 0; i < nodes.getLength(); i++) {
+            org.w3c.dom.Node node = nodes.item(i);
             NamedNodeMap nodeAttributes = node.getAttributes();
-            String userName = nodeAttributes.getNamedItem( "user" ).getNodeValue();
+            String userName = nodeAttributes.getNamedItem("user").getNodeValue();
 
-            Node user = users.computeIfAbsent( userName, name -> mergeNode( USER, "name", name ) );
+            Node user = users.computeIfAbsent(userName, name -> mergeNode(USER, "name", name));
 
-            String pointId = nodeAttributes.getNamedItem( "id" ).getNodeValue();
-            org.neo4j.graphdb.Node point = mergeNode( POINT, "id", pointId );
-            point.setProperty( "latitude", Double.parseDouble( nodeAttributes.getNamedItem( "lat" ).getNodeValue() ) );
-            point.setProperty( "longitude", Double.parseDouble( nodeAttributes.getNamedItem( "lon" ).getNodeValue() ) );
+            String pointId = nodeAttributes.getNamedItem("id").getNodeValue();
+            org.neo4j.graphdb.Node point = mergeNode(POINT, "id", pointId);
+            point.setProperty("latitude", Double.parseDouble(nodeAttributes.getNamedItem("lat").getNodeValue()));
+            point.setProperty("longitude", Double.parseDouble(nodeAttributes.getNamedItem("lon").getNodeValue()));
 
-            mergeRelationship( point, user, EDITED );
+            mergeRelationship(point, user, EDITED);
         }
 
-        return Stream.of( new Summary("OSM data imported") );
+        return Stream.of(new Summary("OSM data imported"));
 
     }
 
-    private void mergeRelationship( org.neo4j.graphdb.Node point, org.neo4j.graphdb.Node user,
-                                    RelationshipType relationshipType )
-    {
-        Relationship editedRelationship = point.getSingleRelationship( relationshipType, Direction.OUTGOING );
-        if(editedRelationship == null)
-        {
-            point.createRelationshipTo( user, relationshipType );
+    private void mergeRelationship(org.neo4j.graphdb.Node point, org.neo4j.graphdb.Node user,
+                                   RelationshipType relationshipType) {
+        Relationship editedRelationship = point.getSingleRelationship(relationshipType, Direction.OUTGOING);
+        if (editedRelationship == null) {
+            point.createRelationshipTo(user, relationshipType);
         }
     }
 
-    private org.neo4j.graphdb.Node mergeNode( Label label, String key, String value )
-    {
-        org.neo4j.graphdb.Node point = db.findNode( label, key, value );
-        if ( point == null )
-        {
-            point = db.createNode( label );
-            point.setProperty( key, value );
+    private org.neo4j.graphdb.Node mergeNode(Label label, String key, String value) {
+        org.neo4j.graphdb.Node point = tx.findNode(label, key, value);
+        if (point == null) {
+            point = tx.createNode(label);
+            point.setProperty(key, value);
         }
         return point;
     }
--- import/custom-procedure/src/test/java/osm/OSMImportProcedureTest.java
+++ import/custom-procedure/src/test/java/osm/OSMImportProcedureTest.java
@@ -1,47 +1,49 @@
 package osm;
 
-import org.junit.Rule;
-import org.junit.Test;
-import osm.OSMImportProcedure;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.harness.Neo4j;
+import org.neo4j.harness.Neo4jBuilders;
 
-import org.neo4j.driver.v1.Config;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
-import org.neo4j.driver.v1.StatementResult;
-import org.neo4j.harness.junit.Neo4jRule;
-
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class OSMImportProcedureTest
 {
-    // This rule starts a Neo4j instance
-    @Rule
-    public Neo4jRule neo4j = new Neo4jRule()
 
-            // This is the Procedure we want to test
-            .withProcedure( OSMImportProcedure.class );
+    private static Neo4j neo4j;
+
+    @BeforeAll
+    static void initializeNeo4j() {
+        neo4j = Neo4jBuilders.newInProcessBuilder()
+                .withDisabledServer() // No need for http
+                .withProcedure(OSMImportProcedure.class)
+        .build();
+    }
 
     @Test
     public void shouldParseXmlFile() throws Throwable
     {
         // In a try-block, to make sure we close the driver after the test
-        try( Driver driver = GraphDatabase.driver( neo4j.boltURI() , Config.build().withoutEncryption().toConfig() ) )
+        try( Driver driver = GraphDatabase.driver( neo4j.boltURI() ) )
         {
             try(Session session = driver.session())
             {
-                StatementResult result = session.run(
-                        "CALL osm.importUri('http://overpass.osm.rambler.ru/cgi/xapi_meta?*[bbox=11.54,48.14,11.543, 48.145]') YIELD value RETURN value"
+                Result result = session.run(
+                        "CALL osm.importUri('https://overpass-api.de/api/xapi_meta?*[bbox=11.54,48.14,11.543, 48.145]') YIELD value RETURN value"
                 );
+                result.consume();
             }
 
             try(Session session = driver.session())
             {
-                StatementResult result = session.run( "MATCH (:Point) RETURN count(*) AS count" );
-                assertEquals(2266, result.peek().get( "count" ).asInt());
+                Result result = session.run( "MATCH (:Point) RETURN count(*) AS count" );
+                assertEquals(2199, result.peek().get( "count" ).asInt());
+                result.consume();
             }
         }
     }
-
 }
